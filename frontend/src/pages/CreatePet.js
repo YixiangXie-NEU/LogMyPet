39c41ce20739ebfce70fe0e73f75a2459e571f49
Yixiang Xie <xie.yix@northeastern.edu>
2022-11-22 01:45:46 -0800

    feat: create pet add api and connect api

--- frontend/src/pages/CreatePet.js
+++ frontend/src/pages/CreatePet.js
@@ -1,62 +1,136 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import "../assets/styles/Colors.css";
+import RadioGroup from "../components/RadioGroup";
+
 import "../assets/styles/Buttons.css";
+import "../assets/styles/Colors.css";
 import "../assets/styles/CreatePet.css";
 
-function CreatePet() {
-  const [result, setResult] = useState();
+const CreatePet = () => {
+  const navigate = useNavigate();
 
-  const getValue = async () => {
-    const res = await fetch("/api/helloworld");
-    setResult(await res.json());
+  const [name, setName] = useState("");
+  const [species, setSpecies] = useState("");
+  const [breed, setBreed] = useState("");
+  const [color, setColor] = useState("");
+  const [gender, setGender] = useState("");
+  const [neutered, setNeutered] = useState("");
+  const [birthday, setBirthday] = useState("");
+  const [personality, setPersonality] = useState("");
+  const [alert, setAlert] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch("/api/pets", {
+      method: "GET",
+      body: JSON.stringify({
+        userId: "00000000001",
+        name: name,
+        species: species,
+        breed: breed,
+        color: color,
+        gender: gender,
+        neutered: neutered,
+        birthday: birthday,
+        personality: personality,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      if (res.ok) {
+        navigate("/");
+      } else {
+        setAlert("Failed to create new pet.");
+      }
+    });
   };
 
-  useEffect(() => {
-    getValue();
-  }, []);
-
   return (
-    <div className="d-flex justify-content-center align-items-center container-fluid vh-100 p-0 background-gray">
-      <div className="d-flex flex-column create-pet">
+    <div className="d-flex justify-content-center container-fluid vh-100 background-gray-light create-pet">
+      <div className="d-flex flex-column create-pet-field">
         <h1 className="create-pet-title">Tell us about your pet</h1>
-        <h1 className="create-pet-title">{result}</h1>
-        <form>
+        {alert && (
+          <div
+            className="alert alert-danger d-flex align-items-center"
+            role="alert"
+          >
+            <div>{alert}</div>
+          </div>
+        )}
+        <form onSubmit={handleSubmit}>
           <input
             type="text"
-            className="form-control create-pet-input"
+            className="form-control create-pet-text-input"
             placeholder="Name"
             aria-label="Name"
+            value={name}
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
           ></input>
           <input
             type="text"
-            className="form-control create-pet-input"
+            className="form-control create-pet-text-input"
             placeholder="Species"
             aria-label="Species"
+            value={species}
+            onChange={(event) => {
+              setSpecies(event.target.value);
+            }}
           ></input>
           <input
             type="text"
-            className="form-control create-pet-input"
+            className="form-control create-pet-text-input"
             placeholder="Breed"
             aria-label="Breed"
+            value={breed}
+            onChange={(event) => {
+              setBreed(event.target.value);
+            }}
           ></input>
           <input
             type="text"
-            className="form-control create-pet-input"
+            className="form-control create-pet-text-input"
             placeholder="Color"
             aria-label="Color"
+            value={color}
+            onChange={(event) => {
+              setColor(event.target.value);
+            }}
           ></input>
-
+          <RadioGroup
+            title="Gender"
+            options={["Male", "Female"]}
+            value={gender}
+            onChange={setGender}
+          />
+          <RadioGroup
+            title="Spayed/Neutered"
+            options={["Yes", "No"]}
+            value={neutered}
+            onChange={setNeutered}
+          />
           <input
             type="text"
-            className="form-control create-pet-input"
+            className="form-control create-pet-text-input"
             placeholder="Birthday"
             aria-label="PetBirthday"
+            value={birthday}
+            onChange={(event) => {
+              setBirthday(event.target.value);
+            }}
           ></input>
           <textarea
-            className="form-control create-pet-input"
+            className="form-control create-pet-text-input"
             placeholder="Personality"
             aria-label="Personality"
+            value={personality}
+            onChange={(event) => {
+              setPersonality(event.target.value);
+            }}
           ></textarea>
           <div className="d-flex flex-column button-section">
             <button
@@ -66,7 +140,8 @@ function CreatePet() {
               Cancel
             </button>
             <button
-              type="button"
+              type="submit"
+              value="Submit"
               className="medium-button purple-solid create-pet-create-button"
             >
               Create
@@ -76,6 +151,6 @@ function CreatePet() {
       </div>
     </div>
   );
-}
+};
 
 export default CreatePet;
