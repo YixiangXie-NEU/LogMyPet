e6fc45291a3dc3f770263012303cf539061d9779
jerryasala <97819243+jerryasala@users.noreply.github.com>
2022-12-01 03:12:37 -0800

    Add comment to RightPanelProfile.js
    Provided feedback on user profile editing functionality

--- frontend/src/components/RightPanelProfile.js
+++ frontend/src/components/RightPanelProfile.js
@@ -13,6 +13,10 @@ const RightPanelProfile = () => {
         className="right-pane-profile-pic"
         alt="A example picture for user profile."
       ></img>
+      /* It seems like the user profile on the right is set to a user
+         Jasmine. I also noticed in the deployed version that I was
+         unable to edit the profile. It might be a good idea to link the
+         user profile info to whoever is logged in. */
       <div className="right-pane-user-name">Jasmine</div>
       <button className="small-button right-pane-edit-profile-button purple-solid">
         Edit profile
